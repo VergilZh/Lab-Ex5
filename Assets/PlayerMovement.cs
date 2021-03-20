c15f4ca44c7a98fcae97ad23d67e0f8778eda886
VergilZh <z1205067147@gmail.com>
2021-03-20 15:08:23 -0400

    Add Scenes Participation 7

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public Rigidbody2D rb;
+    public Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        MoveCharacter(movement);
+    }
+
+    void MoveCharacter(Vector2 direction)
+    {
+        rb.AddForce(direction * speed);
+    }
+}
